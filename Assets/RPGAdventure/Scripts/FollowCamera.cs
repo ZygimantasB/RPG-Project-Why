e97e6b0bfc818c8923ccf8a8d379dd4779892fab
Zygimantas <brinkis.zygimantas@gmail.com>
2021-11-17 22:11:31 +0200

    Initial commit

---
+++ Assets/RPGAdventure/Scripts/FollowCamera.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] // priverčia public tapti private
+    private Transform target; //Jei aš padarau private aš negaliu pasiketi target iš Unity
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (!target) //Jei mes neturime taikinio mums jį reikia grąžinti, jei turi taikinį jį grąžink
+        {
+            return;
+        }
+
+
+        float currentRotationAngle = transform.eulerAngles.y; // kampas, kuriame esu // kamera sukasi rotacijoje
+        float wanterRoatationAngle = target.eulerAngles.y;  // Rotacija į kurią aš nueiti (norima rotacija) // mūsų 
+
+       
+
+        //linijinis interpoliacijos metodas, kur tu gali interpeliuoti reik6mes, tarp dviejų reikšmių
+        currentRotationAngle = Mathf.LerpAngle( // jis duos tarpines reikšmes tarp mano p[radinės reikšmės į reikšmė, kurioje norėčiau būti 
+                                                // iš pradžių aš būsoi 90 > 95 > 100 > 105 > 110, visksa keisis po vieną kadrą,
+                                                // iki mano reikšmės bus nueita per kelis žingsnius, net per vieną
+                                                //tokiu atvėju mano judeliai taps daug lengvesniom švaresni
+            currentRotationAngle, //a mūsų dabartinė reikšmė
+            wanterRoatationAngle,  //b reikšmė, kurioje aš norėčiau būti
+            0.5f);    // t koks didelis pakeitimas bus tarp dviejų reikšmių, kuo mažesnis skaičius, tuo mano kamera judės tolygiau švariau
+                        // 0 mano rotacija nepasikeis
+                        
+        transform.position = new Vector3(
+            target.position.x,
+            5.0f,
+            target.position.z);
+
+        // currentRotationAngle laipsnis pasukamas aplink Y ašį
+        Quaternion currentRotation = Quaternion.Euler(0, currentRotationAngle, 0); // currentRotation norės pasakyti, kiek mes norėsime pasisukti. 
+
+        //pasukta vektorių pirmyn currentRotationAngle kampo laipsniu aplink Y ašį
+        Vector3 rotatedPosition = currentRotation * Vector3.forward; 
+
+        transform.position -= rotatedPosition * 10; // mano kamera nuo žaidėjo bus atgal 10 vienetų
+
+        transform.LookAt(target);
+    }
+}
+ 
---
+++ Assets/RPGAdventure/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPGAdventure
+{
+    public class PlayerController : MonoBehaviour
+    {
+        public float speed;
+        public float roatationSpeed;
+
+        private Vector3 m_Movement;
+        private Rigidbody m_Rb;
+        private Quaternion m_Rotation; // Rotacija yra laikoma Quaternion
+
+
+
+        private void Start()
+        {
+            m_Rb = GetComponent<Rigidbody>();
+
+            var camera = GetComponent<FollowCamera>(); //Neveikia
+
+        }
+        void FixedUpdate()
+        {
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
+
+            m_Movement = new Vector3(horizontalInput, 0, verticalInput);
+            m_Movement.Normalize();
+
+            Vector3 desiredForward = Vector3.RotateTowards( // Mano Player stengsis žiūrėti į priekį
+                transform.forward,
+                m_Movement,
+                Time.fixedDeltaTime * roatationSpeed,
+                0);
+
+
+            m_Rotation = Quaternion.LookRotation(desiredForward);
+
+            m_Rb.MovePosition(m_Rb.position + m_Movement * speed * Time.deltaTime); //MovePostition naudojamas, kai yra
+            m_Rb.MoveRotation(m_Rotation);                                          //pridedamas ridgetbody ir norima kad mano player
+                                                                                    //suktūsi aplinkui x ir negali pasisukti, nes 
+                                                                                    // kitu atvėju nugriūs (aš padariau frozen)
+
+
+            
+
+        }
+    }
+}
+
+
